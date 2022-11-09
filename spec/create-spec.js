1a623f6d3350ec84ebeff2ac0f9c577d8d6d5f85
Alan Smith <alan.smith@thenbs.com>
2022-11-09 10:49:51 +0000

    Integrate changes from Claudia pull requests

--- spec/create-spec.js
+++ spec/create-spec.js
@@ -20,8 +20,8 @@ describe('create', () => {
 
 
 	let workingdir, testRunName, iam, lambda, s3, newObjects, config, logs, apiGatewayPromise, sns;
-	const defaultRuntime = 'nodejs14.x',
-		supportedRuntimes = ['nodejs14.x', 'nodejs12.x', 'nodejs10.x'],
+	const defaultRuntime = 'nodejs16.x',
+		supportedRuntimes = ['nodejs16.x', 'nodejs14.x', 'nodejs12.x'],
 		createFromDir = function (dir, logger) {
 			if (!fs.existsSync(workingdir)) {
 				fs.mkdirSync(workingdir);
